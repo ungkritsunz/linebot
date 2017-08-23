132d38d04e1f54f92aca2b207186324cc9650b66
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 16:46:26 +0700

    q

---
+++ showtable.php
@@ -0,0 +1,21 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);			
+$conn = new mysqli($server, $username, $password, $db);
+	$sql = "SELECT id, ask, ans FROM detail";
+	$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+            echo "<table style="width:100%">";
+            while($row = $result->fetch_assoc()) {
+                    echo ="<tr>"
+                    echo ="<td>'$row["ask"]'</td>"
+                    echo ="<td>'$row["ans"]'</td>"
+                    echo "</tr>"
+            }
+            echo "</table>";
+        }        
+$conn->close();
+?>
