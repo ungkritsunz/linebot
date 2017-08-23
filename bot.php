c4ae6db0c636d2a7bcaa51212ba4bba43e63d7c2
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 10:43:04 +0700

    ๆๆๆ

--- bot.php
+++ bot.php
@@ -1,12 +1,7 @@
 <?php
 $access_token = 'y3aNFkkeuf8tR8fXhNQU0LvyrfM3Vhw0So3PjsQ1gxNh/5wKOJFABxLtZgezsePRNZEm7QocgsYopcv7vH4Lr+9Lz806DgeCTpeFKas8xayGjMlYqd4lUMCaaDWIOwUiWc2AhEiLnUFHFyp9pYvAFAdB04t89/1O/w1cDnyilFU=';
 $arr = array('ดีจ้า' => 'ดีคับ');
-$ans = 'hello';
-$ask = 'bello';
-$newArr = array($ans=>$ask);
-$resultMerge = array_merge($arr, $newArr);
-$responseMessage = array_search('ดีคับ', $resultMerge);
-echo $responseMessage;
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
