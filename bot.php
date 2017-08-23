3877fcf33eb305813c0009fb4632c1022905b0ea
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 11:26:36 +0700

    q

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
                   }
 
             }else{  
-                $text = 'ไม่รู้จักจ้า'.' + '.$event['message']['type'].' + '.$responseMessage;
+                $text = 'ไม่รู้จักจ้า + '.$event['message']['type'].' + '.$responseMessage;
             }
             
 			// Get replyToken
