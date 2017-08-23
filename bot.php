1242c3f97ccb011eec614d2946710b7687cda8a7
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 09:04:26 +0700

    หห

--- bot.php
+++ bot.php
@@ -19,14 +19,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "ตอบบ"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => "ตอบ",
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
