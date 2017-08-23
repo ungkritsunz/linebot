43b9cd09ea2b6345712a20b0e7c4329d8dbfefd0
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 09:17:03 +0700

    qwe

--- bot.php
+++ bot.php
@@ -13,8 +13,8 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
             // $text = $event['message']['text'];
-            $text = "sssss";
-            if($event['type'] != 'message'){
+            $text = "ssssszzz";
+            if($event['message']['type'] != 'text'){
                 $text = "aaaaa";
             }
 			// Get replyToken
