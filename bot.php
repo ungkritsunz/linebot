71bad91780cb4681ac4c1b25211743130d44063b
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 15:55:47 +0700

    q

--- bot.php
+++ bot.php
@@ -1,15 +1,4 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-
-$conn = new mysqli($server, $username, $password, $db);
-$sql = "SELECT id, ask, ans FROM detail";
-$result = $conn->query($sql);
 $text='';
 $access_token = 'y3aNFkkeuf8tR8fXhNQU0LvyrfM3Vhw0So3PjsQ1gxNh/5wKOJFABxLtZgezsePRNZEm7QocgsYopcv7vH4Lr+9Lz806DgeCTpeFKas8xayGjMlYqd4lUMCaaDWIOwUiWc2AhEiLnUFHFyp9pYvAFAdB04t89/1O/w1cDnyilFU=';
 
@@ -19,31 +8,27 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 // Validate parsed JSON data
 
-if ($result->num_rows > 0) {
-	// output data of each row
-	while($row = $result->fetch_assoc()) {
-
-		echo "id: " . $row["id"]. " - ask: " . $row["ask"]. " " . $row["ans"]. "<br>";
-	}
-} 
 
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-            // $text = $event['message']['text'];
-            //$inputText = $event['message']['type'];
-			//$responseMessage = array_search($event['message']['text'], $arr);
+			$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 			
+			$server = $url["host"];
+			$username = $url["user"];
+			$password = $url["pass"];
+			$db = substr($url["path"], 1);
+			
+			
+			$conn = new mysqli($server, $username, $password, $db);
+			$sql = "SELECT id, ask, ans FROM detail";
+			$result = $conn->query($sql);
+
 			if ($result->num_rows > 0) {
-				// output data of each row
 				while($row = $result->fetch_assoc()) {
-					//if($row["ask"]==$event['message']['text']){
 						$text = $row["ask"].' : '.$event['message']['text'];
-					//}
-					//echo "id: " . $row["id"]. " - ask: " . $row["ask"]. " " . $row["ans"]. "<br>";
 				}
 			} 
 			// if($text==''){
