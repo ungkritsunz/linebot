ae48483afd01c1a8af954df966dd2b982199ac47
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 17:07:08 +0700

    q

--- showtable.php
+++ showtable.php
@@ -6,11 +6,11 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);			
 $conn = new mysqli($server, $username, $password, $db);
 $sql = "SELECT id, ask, ans FROM detail";
-	$result = $conn->query($sql);
+$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
             echo '<table class="table-bordered">';
             echo '<tbody>';
-                while ($row = $result->fetch_object()) {
+                while ($row = $result->fetch_assoc()) {
                     echo '<tr>';
                         echo '<td>'.$row["ask"].'</td><td>'.$row["ans"]."</td>";
                     echo '</tr>';
