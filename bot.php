97271f36be334f7d1311a39f7a3846f47a1aa856
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 16:02:46 +0700

    q

--- bot.php
+++ bot.php
@@ -1,13 +1,10 @@
 <?php
-			$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-			
-			$server = $url["host"];
-			$username = $url["user"];
-			$password = $url["pass"];
-			$db = substr($url["path"], 1);
-			
-			
-			$conn = new mysqli($server, $username, $password, $db);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);			
+$conn = new mysqli($server, $username, $password, $db);
 
 $text='';
 $access_token = 'y3aNFkkeuf8tR8fXhNQU0LvyrfM3Vhw0So3PjsQ1gxNh/5wKOJFABxLtZgezsePRNZEm7QocgsYopcv7vH4Lr+9Lz806DgeCTpeFKas8xayGjMlYqd4lUMCaaDWIOwUiWc2AhEiLnUFHFyp9pYvAFAdB04t89/1O/w1cDnyilFU=';
@@ -28,20 +25,21 @@ if (!is_null($events['events'])) {
 			$result = $conn->query($sql);
 			if ($result->num_rows > 0) {
 				while($row = $result->fetch_assoc()) {
-					
-						$text = $row["ask"].' : '.$event['message']['text'];
+					if($row["ask"]==$event['message']['text']){
+						$text = $row["ans"];
+					}
 				}
 			} 
-			// if($text==''){
-			// 	if(strpos($event['message']['text'],"==")>0){
-			// 		$findStr = strpos($event['message']['text'],"==");
-			// 		$subStrAns = substr($event['message']['text'],0,$findStr);
-			// 		$subStrAsk = substr($event['message']['text'],$findStr+2);
-			// 		$text = 'ok, I remember.';
-			// 	}else{
-			// 		$text = 'I dont know -- :'.$event['message']['text'];
-			// 	  }				
-			// }       
+			if($text==''){
+				if(strpos($event['message']['text'],"==")>0){
+					$findStr = strpos($event['message']['text'],"==");
+					$subStrAns = substr($event['message']['text'],0,$findStr);
+					$subStrAsk = substr($event['message']['text'],$findStr+2);
+					$text = 'ok, I remember.';
+				}else{
+					$text = 'I dont know -- :'.$event['message']['text'];
+				  }				
+			}       
             
 			// Get replyToken
 			$replyToken = $event['replyToken'];
