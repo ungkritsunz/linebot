a7212221701fd704c0b73645ecfaf160a23c0e99
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-23 15:07:01 +0700

    q

--- bot.php
+++ bot.php
@@ -1,4 +1,17 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+
+$conn = new mysqli($server, $username, $password, $db);
+$sql = "SELECT id, ask, ans FROM detail";
+$result = $conn->query($sql);
+echo $result;
+
 $access_token = 'y3aNFkkeuf8tR8fXhNQU0LvyrfM3Vhw0So3PjsQ1gxNh/5wKOJFABxLtZgezsePRNZEm7QocgsYopcv7vH4Lr+9Lz806DgeCTpeFKas8xayGjMlYqd4lUMCaaDWIOwUiWc2AhEiLnUFHFyp9pYvAFAdB04t89/1O/w1cDnyilFU=';
 $arr = array('ไม่บอกอิอิ' => 'ชื่อไร');
 $inputText='';
