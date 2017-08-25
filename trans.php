72b6f536ea90ac9151763106207c0821a20e2a69
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-25 07:58:54 +0700

    q

---
+++ trans.php
@@ -0,0 +1,30 @@
+<?php
+ require_once "GoogleTranslate.php";
+ $word = $_REQUEST['word'];
+$GT = NEW GoogleTranslate();
+$response = $GT->translate('th','en',$word);  /// ตรง en เราสามารถเปลี่ยนเป็น ภาษาอื่นได้
+//echo "<pre>";
+echo $word."   =   ".$response;
+?>
+
+/// ตรง en เราสามารถเปลี่ยนเป็น ภาษาอื่นได้  เช่น เกาหลี = ko ,สเปน = sp
+
+//// แปลไทยเป็น ญี่ปุ่น
+
+//  require_once "GoogleTranslate.php";
+//  $word = $_REQUEST['word'];
+// $GT = NEW GoogleTranslate();
+// $response = $GT->translate('th','ja',$word);
+// //echo "<pre>";
+// echo $word."   =   ".$response;
+
+
+//// แปลเป็นอังกฤษไทย 
+
+
+//  require_once "GoogleTranslate.php";
+//  $word = $_REQUEST['word'];
+// $GT = NEW GoogleTranslate();
+// $response = $GT->translate('en','th',$word);
+// //echo "<pre>";
+// echo $word."   =   ".$response;
