a3422aced16538c82f38b5c15eb6972beede0ccd
Ungkrit Jullawut <se57160162@gmail.com>
2017-08-25 14:53:19 +0700

    ๆ

--- bot.php
+++ bot.php
@@ -41,10 +41,6 @@ if (!is_null($events['events'])) {
 							case "กินไรดี":
 								array_push($textEat,$row["ans"]);
 								break;
-							case "รูป":
-								$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
-								$text = new LINE\LINEBot\MessageBuilder\ImageMessageBuilder($img_url, $img_url);
-								break;
 							default:
 								$text = $row["ans"];
 						}
