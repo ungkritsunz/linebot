d5a1cab08972f160dae2c5abcbbe55e0c6c5a23a
Ungkrit Jullawut <se57160162@gmail.com>
2018-03-20 01:35:24 +0700

    ๆๆๆๆๆ

--- bot.php
+++ bot.php
@@ -41,6 +41,26 @@ if (!is_null($events['events'])) {
 						
 						switch ($row["ask"]) {
 							case "เปิดพัดลม":
+								$url = 'http://localhost/test.php'; 
+								
+								$data = "fn=login&test=1";
+								
+								try{
+								$ch = curl_init();
+								curl_setopt( $ch, CURLOPT_URL, $url );
+								curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+								curl_setopt( $ch, CURLOPT_POST, true );
+								curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+								curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+								$content = curl_exec( $ch );
+								curl_close($ch);
+								
+								print_r($content);
+								
+								}catch(Exception $ex){
+								
+								echo $ex;
+								}
 							$text = "เปิดแล้วงับ";
 								break;
 							case "งาน":
