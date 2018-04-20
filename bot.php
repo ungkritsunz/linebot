094566caa330be36d2bf090042aff6fe76b334dc
Ungkrit Jullawut <se57160162@gmail.com>
2018-04-20 09:16:48 +0700

    eee

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ $textEat=array();
 $access_token = 'y3aNFkkeuf8tR8fXhNQU0LvyrfM3Vhw0So3PjsQ1gxNh/5wKOJFABxLtZgezsePRNZEm7QocgsYopcv7vH4Lr+9Lz806DgeCTpeFKas8xayGjMlYqd4lUMCaaDWIOwUiWc2AhEiLnUFHFyp9pYvAFAdB04t89/1O/w1cDnyilFU=';
 $checkWord = "";
 // Fix Ip Here
-$publicIp001 = "113.53.90.14";
+$publicIp001 = "1.4.205.63";
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
