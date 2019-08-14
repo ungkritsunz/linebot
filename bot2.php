70b7b1a744bce94f78b282ce4b434a4cf6c9ddd7
sunsosay <5801@cdg.co.th>
2019-08-14 13:47:21 +0700

    tes push

--- bot2.php
+++ bot2.php
@@ -46,9 +46,13 @@ if (!is_null($events['events'])) {
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
+			// $data = [
+			// 	'replyToken' => $replyToken,
+			// 	'messages' => [$messages],
+			// ];
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => "test push",
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
